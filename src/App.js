065dc0c71d5704c8675e7e3a6c68e5640a85fde0
maryammalek1365 <maryam.malekhoseyni1@gmail.com>
2025-10-04 18:32:06 +0330

    ichang

--- src/App.js
+++ src/App.js
@@ -9,28 +9,37 @@ import { About } from "./pages/About";
 import { Contact } from "./pages/Contact";
 import { Nav } from "./pages/Nav";
 import { Profile } from "./pages/Profile";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+
 
 export const profileContext = createContext();
 
 function App() {
     const [username, setUsername] = useState("sarvin")
+    const client = new QueryClient({
+        defaultOptions: {
+            queries: { refetchOnWindowFocus: false }
+        }
+    })
     return (
         <div className="App">
-
-            <profileContext.Provider value={{ username, setUsername }} >
-                <Router>
-                    <div> sarvin style </div>
-                    <Nav />
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/about" element={<About />} />
-                        <Route path="/contact" element={<Contact />} />
-                        <Route path="/profile" element={<Profile />} />
-                        <Route path="*" element={<div>not found</div>} />
-                    </Routes>
-                    <div>this is footer</div>
-                </Router>
-            </profileContext.Provider>
+            <QueryClientProvider client={client}>
+                <profileContext.Provider value={{ username, setUsername }} >
+                    <Router>
+                        <div> sarvin style </div>
+                        <Nav />
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/about" element={<About />} />
+                            <Route path="/contact" element={<Contact />} />
+                            <Route path="/profile" element={<Profile />} />
+                            <Route path="*" element={<div>not found</div>} />
+                        </Routes>
+                        <div>this is footer</div>
+                    </Router>
+                </profileContext.Provider>
+            </QueryClientProvider>
         </div>
     );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,24 @@
+import { useQuery } from '@tanstack/react-query'
+import axios from 'axios'
+
 export const Home = (props) => {
-    return <div>  home page -user name is {props.username}</div>
+    const { data: catdata, isLoading, error, isError, refetch } = useQuery({
+        queryKey: ['cat'],
+        queryFn: async () => {
+            const res = await axios.get('https://catfact.ninja/fact')
+            return res.data
+        },
+    })
+
+    if (isLoading) return <p>Loading...</p>
+    if (isError) return <p>Sorry there was an error:error:{error.message}!</p>
+
+    return (
+        <div>
+            <h2>Home Page</h2>
+            <p>User name is: {props.username}</p>
+            <p>🐱 Cat fact: {catdata?.fact}</p>
+            <button onClick={refetch}> up date</button>
+        </div>
+    )
 }
